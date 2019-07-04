9531a309eb6ec5b6411d353a7756ae1cf9a0ccc3
larsfinlay <larsfinlay@googlemail.com>
2019-07-04 10:52:04 +0100

    Train name :: Red

---
+++ TrainStop/Train.cs
@@ -0,0 +1,16 @@
+﻿namespace TrainStop
+{
+    public class Train
+    {
+        private string name;
+
+        public Train(string name)
+        {
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ TrainStopTests/TrainTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainStop;
+
+namespace TrainStopTests
+{
+    [TestClass]
+    public class TrainTests
+    {
+
+        Train train;
+        string name = "SWR820";
+
+        [TestInitialize]
+        public void setUp()
+        {
+            train = new Train(name);
+        }
+
+        [TestMethod]
+        public void setNameTest()
+        {
+            Assert.AreEqual(name, train.getName());
+        }
+
+    }
+}
