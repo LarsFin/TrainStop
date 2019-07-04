863242e02a529ebbb5873bde8a2069df050493ae
larsfinlay <larsfinlay@googlemail.com>
2019-07-04 11:37:13 +0100

    Station has name :: Red

---
+++ TrainStop/Station.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TrainStop
+{
+    public class Station
+    {
+        private string name;
+
+        public Station(string name)
+        {
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ TrainStopTests/StationTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainStop;
+
+namespace TrainStopTests
+{
+    [TestClass]
+    public class StationTests
+    {
+
+        Station station;
+        string name = "London Waterloo";
+
+        [TestInitialize]
+        public void setUp()
+        {
+            station = new Station(name);
+        }
+
+        [TestMethod]
+        public void setNameTest()
+        {
+            Assert.AreEqual(name, station.getName());
+        }
+
+    }
+}
